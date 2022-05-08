86d6833e3e668ee182a6973db7230f2057089f1c
dogudulupcu <dogudulupcu@gmail.com>
2022-05-08 14:54:34 +0300

    First

---
+++ Assets/move.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class move : MonoBehaviour
+{
+    [SerializeField]
+    public Vector3 targetPos1;
+    [SerializeField]
+    public Vector3 targetPos2;
+    [SerializeField]
+    public float speed = 1f;
+    public bool canMove;
+    public bool firstMove;
+    void Start()
+    {
+     
+        firstMove = true;
+     
+    }
+    void Update()
+    {
+        if(transform.position == targetPos1)
+        {
+            firstMove = false;
+        }
+        if (transform.position == targetPos2)
+        {
+            firstMove = true;
+        }
+        if (canMove)
+        {
+            if (firstMove)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos1, speed* Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos2, speed* Time.deltaTime);
+            }
+        }
+     
+    }
+}
